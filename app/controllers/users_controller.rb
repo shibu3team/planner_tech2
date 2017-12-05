d064977b91dc69598f6d981dbbd8dfea2259af89
shibu3team <jg201038@gmail.com>
2017-12-05 20:45:57 +0900

    user_new_page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: "登録が完了しました"
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      render :new
+    end
+  end
+
+
+  def index
+    @user = User.find_by(id:params[:id])
+
+  end
+
+  def edit
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
+
+
+end
